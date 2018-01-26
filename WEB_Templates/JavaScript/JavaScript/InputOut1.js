68a6e968eb23a9f94cb62281f4be6a5fcf3c0c18
Ruslanito <breitovo76@gmail.com>
2018-01-26 17:30:50 +0300

    add InputOut1 in JS

---
+++ WEB_Templates/JavaScript/JavaScript/InputOut1.js
@@ -0,0 +1,20 @@
+function out() {
+   var p;
+   p= document.getElementById("out1");
+   //p.innerHTML = "Hello!";
+   //дописать информацию
+   p.innerHTML +=" <b> Hello</b> ";
+}
+
+function outnew(){
+   var p =document.getElementById("out1");
+
+   p.innerHTML = "Hello! Here I am !";
+}
+
+function outnothing() {
+   var p =document.getElementById("out1");
+
+   p.innerHTML = "";
+
+}
