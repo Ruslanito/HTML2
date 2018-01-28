023dd01743fa750a76c85e93193a7e79496b1e16
Ruslanito <breitovo76@gmail.com>
2018-01-28 21:30:20 +0300

    add innerHTML in inputOut2 in JavaScript

--- WEB_Templates/JavaScript/JavaScript/InputOut2.js
+++ WEB_Templates/JavaScript/JavaScript/InputOut2.js
@@ -2,3 +2,21 @@ function change(){
    var p =document.getElementById('out');
    p.innerHTML = "Hello! Text#2";
 }
+
+function change2(){
+   var r = document.getElementById('out2');
+   r.innerHTML = r.innerHTML+" + Text#3";
+   //r.innerHTML+= " + Text#3";
+}
+
+function change3(){
+   var s = document.getElementById('out3');
+   s.innerHTML = s.innerHTML+" + <b> Text#3</b>";
+
+}
+
+function change4(){
+   var z = document.getElementById('out4');
+   z.innerText = z.innerText+" + <b> Text#4</b>";
+
+}
