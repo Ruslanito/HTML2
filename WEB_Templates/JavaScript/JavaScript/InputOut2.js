3b54c294ee862a9a0f1233235bee17b7ed2fae2a
Ruslanito <breitovo76@gmail.com>
2018-01-28 21:54:13 +0300

    add innerHTML in inputOut2 and insertAsjacentHTML in JavaScript

--- WEB_Templates/JavaScript/JavaScript/InputOut2.js
+++ WEB_Templates/JavaScript/JavaScript/InputOut2.js
@@ -12,11 +12,14 @@ function change2(){
 function change3(){
    var s = document.getElementById('out3');
    s.innerHTML = s.innerHTML+" + <b> Text#3</b>";
-
 }
 
 function change4(){
    var z = document.getElementById('out4');
    z.innerText = z.innerText+" + <b> Text#4</b>";
+}
 
+function change5(){
+   var a = document.getElementById('out5');
+   a.insertAdjacentHTML("beforeEnd","Text#5");
 }
