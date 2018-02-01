bbe7fc4e43987a986b7c2b14b04c800831fabd02
Ruslanito <breitovo76@gmail.com>
2018-02-01 23:56:32 +0300

    add if-guess_number2 in JS

---
+++ WEB_Templates/JavaScript/JavaScript/if-guess_number2.js
@@ -0,0 +1,26 @@
+var c, temp-out;
+c = Math.floor(Math.random()*10+1);
+temp-out = document.getElementById(temp-out);
+temp-out.innerHTML = c;
+
+
+
+
+function f1(){
+   var d, p;
+
+   d=document.getElementById("num1").value;
+   d= parseInt(d);
+   p=document.getElementById("out")
+
+   if (d==c) {
+      p.innerHTML = "Угадал число";
+   }
+   else if (d<100) {
+      p.innerHTML="Число меньше, чем нужно";
+   }
+   else {
+      p.innerHTML="Число больше, чем нужно";
+   }
+
+}
