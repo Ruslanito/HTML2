f7851b3e96377949559cd07d4b28e5d74a71a277
Ruslanito <breitovo76@gmail.com>
2018-02-02 00:14:24 +0300

    add little changes in if-guess_number2.js in JS

--- WEB_Templates/JavaScript/JavaScript/if-guess_number2.js
+++ WEB_Templates/JavaScript/JavaScript/if-guess_number2.js
@@ -1,10 +1,7 @@
 var c, temp-out;
-c = Math.floor(Math.random()*10+1);
-temp-out = document.getElementById(temp-out);
+c = Math.floor((Math.random()*10)+1);
+temp-out = document.getElementById("temp-out");
 temp-out.innerHTML = c;
-
-
-
 
 function f1(){
    var d, p;
@@ -16,7 +13,7 @@ function f1(){
    if (d==c) {
       p.innerHTML = "Угадал число";
    }
-   else if (d<100) {
+   else if (d<c) {
       p.innerHTML="Число меньше, чем нужно";
    }
    else {
