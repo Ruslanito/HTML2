54b64611720bccaea68ff7f0d06d2ed84d45f536
Ruslanito <breitovo76@gmail.com>
2018-02-02 17:14:42 +0300

    add array2 in JS

--- WEB_Templates/JavaScript/JavaScript/array2.js
+++ WEB_Templates/JavaScript/JavaScript/array2.js
@@ -1,17 +1,33 @@
-var m = [];
-var m1 = [23,56,"hi"];
+var m1 = [23, 56, "hi"];
 //вывод массива
-console.log (m1,[2]);
+console.log(m1, [2]);
 console.log(m1);
 // длина массива length
-alert (m1.length);
+alert(m1.length);
 //добавление элемента
 m1[3] = "Hello";
 console.log(m1);
-alert (m1.length);
 
-var t = document.getElementById("out");
+function masOut() {
+   var p = document.getElementById("out");
+   var str = '';
+   for (var i = 0; i < m1.length; i++) {
+      str += i + '---' + m1[i] + '<br>';
+   }
+   p.innerHTML = str;
 
-for (var i = 0; i < m1.length; i++) {
-   t.innerHTML+=[m1[i]]+' <br> ';
+}
+
+//запуск функции при старте
+masOut();
+
+function p1() {
+   var i1 = document.getElementById('i1').value;
+   m1.push(i1);
+   masOut();
+}
+
+function p2() {
+   m1.pop();
+   masOut();
 }
