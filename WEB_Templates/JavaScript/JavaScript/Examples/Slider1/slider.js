220c8def2375a4c0a99339b55822d053a367f33c
Ruslanito <breitovo76@gmail.com>
2018-02-09 23:17:53 +0300

    remove errors in slider.js

--- WEB_Templates/JavaScript/JavaScript/Examples/Slider1/slider.js
+++ WEB_Templates/JavaScript/JavaScript/Examples/Slider1/slider.js
@@ -1,6 +1,7 @@
 document.getElementById('slider-left').onclick = sliderLeft;
 
-function sliderLeft(
-   var lines = document.getElementById('lines');
-   lines.style.left = -128+'px';
-)
+function sliderLeft() {
+    var lines;
+    lines = document.getElementById('lines');
+    lines.style.left = -128 + 'px';
+}
