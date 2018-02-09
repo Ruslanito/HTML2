b841da9052ac81fe4b21f7811be46218d2ac5959
Ruslanito <breitovo76@gmail.com>
2018-02-10 00:14:18 +0300

    slider in JS is ready

--- WEB_Templates/JavaScript/JavaScript/Examples/Slider1/slider.js
+++ WEB_Templates/JavaScript/JavaScript/Examples/Slider1/slider.js
@@ -1,7 +1,12 @@
 document.getElementById('slider-left').onclick = sliderLeft;
+var left = 0;
 
 function sliderLeft() {
     var lines;
     lines = document.getElementById('lines');
-    lines.style.left = -128 + 'px';
+    left = left - 128;
+    if (left < -512) {
+        left = 0;
+    }
+    lines.style.left = left + 'px';
 }
