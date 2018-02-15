3e2f6b5beaa7dae97975489e9f9e33db83f7607a
Ruslanito <breitovo76@gmail.com>
2018-02-15 16:44:38 +0300

    Slider2 is ready in JS

--- WEB_Templates/JavaScript/JavaScript/Examples/Slider2/slider.js
+++ WEB_Templates/JavaScript/JavaScript/Examples/Slider2/slider.js
@@ -1,6 +1,20 @@
 document.getElementById('slider-left').onclick = sliderLeft;
+autoSlider();
 var left = 0;
+var timer;
+
+function autoSlider() {
+    timer = setTimeout(sliderLeft, 1000); /* переметры запуска для просмотра */
+}
 
 function sliderLeft() {
-
+    var lines;
+    lines = document.getElementById('lines');
+    left = left - 128;
+    if (left < -512) {
+        left = 0;
+        clearTimeout(timer);
+    }
+    lines.style.left = left + 'px';
+    autoSlider(); /* зацикливание просмотра таймера */
 }
