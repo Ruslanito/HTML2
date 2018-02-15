837b3ce73b5bc45ed33a12ba8b6262960bb57308
Ruslanito <breitovo76@gmail.com>
2018-02-15 16:53:53 +0300

    slider2-timer w/o button is ready

--- WEB_Templates/JavaScript/JavaScript/Examples/Slider2-timer_w.o_button/slider.js
+++ WEB_Templates/JavaScript/JavaScript/Examples/Slider2-timer_w.o_button/slider.js
@@ -1,20 +1,17 @@
-document.getElementById('slider-left').onclick = sliderLeft;
 autoSlider();
 var left = 0;
 var timer;
 
 function autoSlider() {
-    timer = setTimeout(sliderLeft, 1000); /* переметры запуска для просмотра */
-}
-
-function sliderLeft() {
-    var lines;
-    lines = document.getElementById('lines');
-    left = left - 128;
-    if (left < -512) {
-        left = 0;
-        clearTimeout(timer);
-    }
-    lines.style.left = left + 'px';
-    autoSlider(); /* зацикливание просмотра таймера */
+    timer = setTimeout(function() {; /* переметры запуска для просмотра */
+        var lines;
+        lines = document.getElementById('lines');
+        left = left - 128;
+        if (left < -512) {
+            left = 0;
+            clearTimeout(timer);
+        }
+        lines.style.left = left + 'px';
+        autoSlider(); /* зацикливание просмотра таймера */
+    }, 1000);
 }
