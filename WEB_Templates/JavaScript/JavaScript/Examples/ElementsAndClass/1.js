755a83b50cff043d0177c526ad410eb36f25c29e
Ruslanito <breitovo76@gmail.com>
2018-02-16 23:28:26 +0300

    ass change color in ElementsAndClass in JS

--- WEB_Templates/JavaScript/JavaScript/Examples/ElementsAndClass/1.js
+++ WEB_Templates/JavaScript/JavaScript/Examples/ElementsAndClass/1.js
@@ -1,8 +1,17 @@
 var p = document.getElementsByTagName('p');
 console.log(p);
 // p.onclick = f1; ошибка вызова функции
-p[0].onclick = f1;
+
+for (i = 0; i < p.length; i++) {
+    p[i].onclick = f1;
+}
+
 
 function f1() {
-    alert('Hello');
+    // alert('Hello');
+    console.log(this);
+    // в переменной this лежит вызываемый элемент массива p
+    // изменение (свойства) цвета выбранного элемента
+    this.style.background = 'grey';
+
 }
