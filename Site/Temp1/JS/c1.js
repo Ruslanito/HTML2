1aeee275e86b0430380e5a78569d69e033899a28
Ruslanito <breitovo76@gmail.com>
2018-03-06 12:14:50 +0300

    create c1.js in Site/Temp1/JS

---
+++ Site/Temp1/JS/c1.js
@@ -0,0 +1,14 @@
+/* function alert() {
+    alert(
+        "js is running"
+    )
+} */
+
+alert("js is running");
+
+function count() {
+    var n, d, res;
+    n = document.getElementById('q1').value;
+    d = document.getElementById('d1').value;
+
+}
