2aea1653af556f797d69ac96b3b1d3bacc3e13f4
Ruslanito <breitovo76@gmail.com>
2018-03-06 18:33:57 +0300

    work on Site/Temp1

--- Site/Temp1/JS/c1.js
+++ Site/Temp1/JS/c1.js
@@ -1,14 +1,12 @@
-/* function alert() {
-    alert(
-        "js is running"
-    )
-} */
+ /*alert("js is running"); */
+ var n = document.getElementById('q1').value;
+ var d = document.getElementById('d1').value;
+ var res = document.getElementById('c1');
 
-alert("js is running");
+ function count() {
+     var res1 = (113 * n * d) / 10000;
+     res.innerHTML = res1;
+     /* alert(n + d); */
+     /* res.insertAdjacentHTML("afterBegin", res1); */
 
-function count() {
-    var n, d, res;
-    n = document.getElementById('q1').value;
-    d = document.getElementById('d1').value;
-
-}
+ }
