eefe7a7e00a6543b5936dfb2bc7f8f05bd10a335
Ruslanito <breitovo76@gmail.com>
2018-03-13 16:26:48 +0300

    first test for running js in MPK

--- InProcess/MPK/JS/js1.js
+++ InProcess/MPK/JS/js1.js
@@ -1,1 +1,19 @@
-alert('Run JS')
+/* alert('Run JS') */
+
+/* var qualityCert = document.getElementById(hin1).value;
+var invoiceNumber = document.getElementById(hin2);
+var invoiceDate = document.getElementById(hin3); */
+
+function replace() {
+    alert('+++++')
+    var qualityCert = document.getElementById(hin1).value;
+    var invoiceNumber = document.getElementById(hin2).value;
+    var invoiceDate = document.getElementById(hin3).value;
+
+
+
+
+
+
+    alert(qualityCert);
+}
