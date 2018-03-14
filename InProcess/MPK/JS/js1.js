74490440a2a12b82abfc93a0ba7a60ba626ae054
Ruslanito <breitovo76@gmail.com>
2018-03-14 13:19:11 +0300

    add new vars in js1.js MPK

--- InProcess/MPK/JS/js1.js
+++ InProcess/MPK/JS/js1.js
@@ -1,14 +1,18 @@
 /* alert('Run JS') */
 
-/* var qualityCert = document.getElementById(hin1).value;
-var invoiceNumber = document.getElementById(hin2);
-var invoiceDate = document.getElementById(hin3); */
-
 function replace() {
     alert('+++++')
     var qualityCert = document.getElementById("hin1").value;
     var invoiceNumber = document.getElementById("hin2").value;
     var invoiceDate = document.getElementById("hin3").value;
+    var dInvoice = document.getElementById("selday").value;
+    alert(dInvoice);
+    var mInvoice = document.getElementById("selmonth").value;
+    var yInvoice = document.getElementById("selyear").value;
 
-    alert(qualityCert + '-' + invoiceNumber + '-' + invoiceDate);
+
+
+
+    /* alert(qualityCert + '-' + invoiceNumber + '-' + invoiceDate); */
+    /* alert( + '-' + dInvoice + '-' + mInvoice + '-' + yInvoice); */
 }
