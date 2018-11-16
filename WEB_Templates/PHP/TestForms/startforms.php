c17cb693940dd883388c8a4969c6b293915335c4
Ruslanito <breitovo76@gmail.com>
2018-11-16 22:34:33 +0300

    add TestForms in WEB_Templates

---
+++ WEB_Templates/PHP/TestForms/startforms.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Обработка формы</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script> -->
+</head>
+<body>
+    <center>
+        <h1>Пользователь ввел</h1>
+        Введенное имя: 
+        <?php
+        echo $_POST["name1"];
+
+        ?>
+    </center>
+    
+</body>
+</html>
