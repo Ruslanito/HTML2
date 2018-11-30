7a672a961b414cc9a31526da4f88f1779ecbebc2
Ruslanito <breitovo76@gmail.com>
2018-11-30 14:17:30 +0300

    add date &strftime in 1date.php in PHP

--- WEB_Templates/PHP/Date_Time/1date.php
+++ WEB_Templates/PHP/Date_Time/1date.php
@@ -1,8 +1,28 @@
 <?php
- 
+ error_reporting(-1);
+
 $d = getdate(); // использовано текущее время
 foreach ( $d as $key => $val )
 echo "$key = $val\n";
 echo "<hr>Сегодня: $d[mday].$d[mon].$d[year]";
+
+echo date( "Сегодня d.m.y H:i\n" );
+echo "+++\n";
+$day = date(d);
+$day2 = date(l);
+$month = date(F);
+echo "$day $month - $day2\n";
+
+echo("Местное время:\n");
+echo strftime("%A %d %B %Y %H:%M\n");
+setlocale('LC_ALL','');
+echo strftime("%A %d %B %Y %X\n");
+echo strftime("%Z\n");
+$day3 = strftime("%d");
+$month2 = strftime("%B");
+$dayname = strftime("%A");
+$year = strftime("%Y");
+echo"$day3 $month2 $year $dayname";
+
 
 ?>
