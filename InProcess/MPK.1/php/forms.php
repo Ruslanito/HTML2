6890685b3b1499b6a837484757d5455372deff80
Ruslanito <breitovo76@gmail.com>
2018-12-08 15:50:14 +0300

    change lines in forms.php in MPK1

--- InProcess/MPK.1/php/forms.php
+++ InProcess/MPK.1/php/forms.php
@@ -2,3 +2,22 @@
 echo ")))))";
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Свидетельство МПК</title>
+    <link rel="stylesheet" href="Styles/main.css">
+    <!-- <link media="print" href="Styles/print.css" /> -->
+    <!-- <script src="JS/js1.js" defer></script> -->
+    <link rel="shortcut icon" href="Img/Chick_128x128.png" type="image/x-icon">
+</head>
+<body>
+    <center>
+        <p>Hi</p>
+    </center>
+</body>
+</html>
